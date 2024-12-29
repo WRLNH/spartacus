44711c0a6f49e4206fb0ee56c677cfc621b6bbb0
WRLNH <wuyutong030111@gmail.com>
2024-12-29 20:59:35 +0800

    flitered data for usp

--- examples_USP/circle_2D_hypersonic/read_data.py
+++ examples_USP/circle_2D_hypersonic/read_data.py
@@ -1,6 +1,16 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('./data_usp/grid.usp.30000.dat', skiprows=9, delim_whitespace=True)
+data = pd.read_csv('./examples_USP/circle_2D_hypersonic/data_usp/grid.usp.31000.dat', sep='\s+', skiprows=9)
 data = data.to_numpy()
-print(np.max(data[:, 1]))
+
+filtered_data = []
+
+for row in data:
+    if (row[3 : 10].any() != 0):
+        filtered_data.append(row)
+
+filtered_data = np.array(filtered_data)
+print(filtered_data)
+
+np.savetxt('./examples_USP/circle_2D_hypersonic/data_usp/filtered_data.csv', filtered_data, delimiter=',')
