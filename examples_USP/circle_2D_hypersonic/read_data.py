f407428951573f6119df38bb738e2322434d63b2
yonagi <wuyutong5618@stu.xmu.edu.cn>
2024-12-29 19:52:31 +0800

    created read_data.py

--- examples_USP/circle_2D_hypersonic/read_data.py
+++ examples_USP/circle_2D_hypersonic/read_data.py
@@ -1,5 +1,6 @@
 import numpy as np
-import re
+import pandas as pd
 
-data = np.loadtxt('./data_usp/grid.usp.30000.dat', delimiter=re.compile(r'\s'), skiprows=9)
-print(data)
+data = pd.read_csv('./data_usp/grid.usp.30000.dat', skiprows=9, delim_whitespace=True)
+data = data.to_numpy()
+print(np.max(data[:, 1]))
